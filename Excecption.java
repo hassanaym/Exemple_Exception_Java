3c2eeb5646b853323c33c7c46fbe51c40d41f5c6
aymHassan <essadik.it.services@gmail.com>
2021-12-05 20:29:46 +0100

    final version

--- Excecption.java
+++ Excecption.java
@@ -4,13 +4,13 @@ import java.util.Scanner;
 public class Excecption {
     public static int saisie(int max) throws Exception{
         int nb=1;
-        int i;
+ 
         Scanner sc ;
         do{
             System.out.println("Veuillez donner un entier: ");
             try{
                 sc = new Scanner(System.in);
-                i = sc.nextInt();
+                int i = sc.nextInt();
                 sc.close();
                 return i;
             }catch(InputMismatchException e){
@@ -18,14 +18,29 @@ public class Excecption {
                 ++nb;
             }
         }while(nb<=max);
+
         throw new Exception("Saisie incorrecte");
     }
+
+    public static int cube() throws Exception{
+        int c = saisie(2);
+        c = c * c * c;
+        if(c>8){
+            throw new ArithmeticException("Wa bzaf");
+        }
+        return c;
+    }
+
     public static void main(String[] args){
-        try{
-            System.out.println(saisie(3));
+            
+        try  {
+            System.out.println((cube()));
+        }catch(ArithmeticException e){
+            System.out.println(e.getMessage());
         }catch(Exception e){
             System.out.println(e.getMessage());
         }
+        
         
     }
 }
